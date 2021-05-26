8ed1ca8020734f5f848ddae6e5962eb445e476bb
lyleshaw <lyle@hdu.edu.cn>
2021-05-26 11:19:59 +0800

    update constant

--- BackEnd/apps/view/constants.py
+++ BackEnd/apps/view/constants.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from apps.a_common.constants import NATION_MAP, PERMISSION_TYPE_MAP, SEX_MAP, USER_IDENTITY_MAP, HealthCode_Map
+from apps.a_common.constants import PERMISSION_TYPE_MAP, SEX_MAP, HealthCode_Map
 from apps.a_common.response import success_response
 
 constants_router = APIRouter()
@@ -10,10 +10,8 @@ constants_prefix = 'constants'
 @constants_router.get("", summary="用于查询所有的constants")
 async def get_constants():
     data = {
-        'user_identity': USER_IDENTITY_MAP,
         'sex': SEX_MAP,
         'permission_type': PERMISSION_TYPE_MAP,
-        'nation': NATION_MAP,
         'health_code': HealthCode_Map
     }
     return success_response(data)
